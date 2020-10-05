1fd46d7e49515ed9a33f5ead9ccc64a075222638
nico <nico@aleph.engineering>
2020-10-05 09:57:06 -0400

    Test COM port connection

--- app.mjs
+++ app.mjs
@@ -1,4 +1,38 @@
-import WeighingMachineHandler from './src/weighing-machine/weighing-machine-handler.mjs'
+import SerialPort from 'serialport'
+import LogUtil from '../src/util/log-util.js'
 
-var portHandler = new WeighingMachineHandler()
-portHandler.startReading()
+var lastData = null
+
+const port = new SerialPort('COM3', {
+    baudRate: 115200
+})
+
+function log(data) {
+    console.log(data);
+}
+
+port.on("data", function (data) {
+    let strData = data.toString().trim();
+    if (strData !== lastData && strData !== "0") {
+        LogUtil.log("Port data: " + data);
+        lastData = strData;
+    }
+});
+
+port.on("open", function () {
+    log("Port open");
+});
+
+port.on("error", function (error) {
+    LogUtil.log(error);
+});
+
+port.on("close", function () {
+    log("Port closed");
+});
+
+function write() {
+    port.write("P\n");
+}
+
+setInterval(write, 1000);
