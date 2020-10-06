f9aab7a8bd81a9520d177f9a66b327065c408f0b
nico <nico@aleph.engineering>
2020-10-06 10:01:10 -0400

    Add port-reader for showing list of ports

---
+++ src/app/port-reader.mjs
@@ -0,0 +1,25 @@
+import PortHandlerUtil from '../util/port-handler-util.mjs'
+import EnvUtil from '../util/env-util.mjs'
+
+try {
+    const serialPort = PortHandlerUtil.build()
+
+    function write() {
+        serialPort.write("P\n")
+    }
+
+    setInterval(write, EnvUtil.getReadFrequencyInMillis());
+} catch (error) {
+    console.log(error)
+}
+
+
+/*
+PortHandlerUtil.build().then((serialPort) => {
+
+    function write() {
+        serialPort.write("P\n")
+    }
+
+    setInterval(write, EnvUtil.getReadFrequencyInMillis());
+}) */
