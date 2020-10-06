57db22b43d6c6eaac8b3ee031053deff97656bd3
nico <nico@aleph.engineering>
2020-10-06 11:10:22 -0400

    Test automatic port detection

--- src/util/port-handler-util.mjs
+++ src/util/port-handler-util.mjs
@@ -4,32 +4,32 @@ export default class PortHandlerUtil {
 
     static build() {
 
-        let port = PortHandlerUtil.getActivePort()
+        PortHandlerUtil.getActivePort().then((port) => {
+            if (!port) {
+                throw "ALERT: No valid port detected."
+            }
+            let baudRate = parseInt(process.env.BAUD_RATE)
 
-        if (!port) {
-            throw "ALERT: No valid port detected."
-        }
-        let baudRate = parseInt(process.env.BAUD_RATE)
+            const serialPort = new SerialPort(port.path, { baudRate: baudRate })
 
-        const serialPort = new SerialPort(port, { baudRate: baudRate })
+            serialPort.on("data", function (data) {
+                console.log("Data: " + data);
+            });
 
-        serialPort.on("data", function (data) {
-            console.log("Data: " + data);
-        });
+            serialPort.on("open", function () {
+                console.log("Port open");
+            });
 
-        serialPort.on("open", function () {
-            console.log("Port open");
-        });
+            serialPort.on("error", function (error) {
+                console.log(error);
+            });
 
-        serialPort.on("error", function (error) {
-            console.log(error);
-        });
+            serialPort.on("close", function () {
+                console.log("Port closed");
+            });
 
-        serialPort.on("close", function () {
-            console.log("Port closed");
-        });
-
-        return serialPort;
+            return serialPort;
+        })
     }
 
     static getActivePort() {
